15fcf8a4e17a0f8070e96dc363bbf184f61b7f16
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-02-03 21:50:23 +0900

    Update 영어가 싫어요.py

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,5 +1,13 @@
+#dictionary의 key, value 활용
 def solution(numbers):
     num_dict = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
     for k, v in num_dict.items():
+        numbers = numbers.replace(k, str(v))
+    return int(numbers)
+
+#enumerate 활용
+def solution(numbers):
+    num_dict = {}
+    for k, v in enumerate(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
         numbers = numbers.replace(k, str(v))
     return int(numbers)
