a7b7687e7c15dbd57d06b5a5a6951e5ae3cff568
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-02-05 17:19:58 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    number_string = my_string.split()
+    answer = int(number_string[0])
+    for i in range(1, len(number_string), 2):
+        if number_string[i] == '+':
+            answer += int(number_string[i+1])
+        else:
+            answer -= int(number_string[i+1])
+    return answer
