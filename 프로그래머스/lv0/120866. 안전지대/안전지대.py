56907b4b2b7d2c18ba50815fb7e571ed7e849329
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-02-05 21:23:41 +0900

    [level 0] Title: 안전지대, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -1,16 +1,13 @@
-import numpy as np
-from collections import Counter
-
 def solution(board):
-    n = len(board)
-    board_padded = np.pad(board, ((1, 1), (1, 1)), constant_values = -1)
-    danger_array = np.pad(board, ((1, 1), (1, 1)), constant_values = -1)
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            if board_padded[i][j] == 1:
-                for x in range(i-1, i+2):
-                    for y in range(j-1, j+2):
-                        danger_array[x][y] = 1
-    danger_list = danger_array.reshape(1, -1).squeeze()
-    answer = Counter(danger_list)[0]
+    answer = 0
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] == 1:
+                for i in range(max(row-1,0), min(row+2,len(board))):
+                    for j in range(max(col-1,0), min(col+2,len(board))):
+                        if board[i][j] == 1:
+                            continue
+                        board[i][j] = -1
+    for k in board:
+        answer += k.count(0)
     return answer
