47a2803d8ef1a533c206feece056ffd2eeee1440
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-02-06 17:38:37 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -0,0 +1,12 @@
+def solution(lines):
+    table = [set([]) for _ in range(200)]
+    for index, line in enumerate(lines):
+        x1, x2 = line
+        for x in range(x1, x2):
+            table[x + 100].add(index)
+            
+    answer = 0
+    for line in table:
+        if len(line) > 1:
+            answer += 1
+    return answer
