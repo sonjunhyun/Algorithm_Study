441cf25899c064ab13f68361702062bdc9d0afb9
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-02-07 15:51:55 +0900

    Update 문자열 밀기.py

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,3 +1,4 @@
+# 문자열 Slicing
 def solution(A, B):
     for push in range(len(A)):
         if A == B:
@@ -6,4 +7,17 @@ def solution(A, B):
             A = A[-1] + A[:-1]
             push += 1
     return -1
-        
+
+# insert, pop 활용
+def solution(A, B):
+    A, B = list(A), list(B)
+    for push in range(len(A)):
+        if A == B:
+            return push
+        else:
+            A.insert(0, A.pop())
+    return -1
+
+# find 활용
+def solution(A, B):
+    return B*2.find(A)
