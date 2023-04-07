68ea01cbc1efafa41657e802c8b576e5a198361f
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-04-07 15:13:25 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,16 +1,5 @@
 def solution(n, words):
-    answer = [0, 0]
-    stack = [words[0]]
-    
     for i in range(1, len(words)):
-        if words[i] not in stack:
-            if words[i][0] == stack[-1][-1]:
-                stack.append(words[i])
-            else:
-                answer = [i%n+1, i//n+1]
-                break
-        else:
-            answer = [i%n+1, i//n+1]
-            break
-            
-    return answer
+        if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
+            return [i%n+1,i//n+1]
+    return [0,0]
