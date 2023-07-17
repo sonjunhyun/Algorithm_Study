9d255280e1cfa53dbf014e5b7a881c8c2f742e05
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-07-17 19:02:16 +0900

    Update 추억 점수.py
    코드 수정 및 간결화

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -8,3 +8,15 @@ def solution(name, yearning, photo):
                 score += score_list.get(i)  # score에 key값에 해당하는 value 더해준다.
         answer.append(score)    # 합한 score를 answer 리스트에 추가
     return answer
+
+# 코드 간결화
+def solution(name, yearning, photo):
+    answer = []
+    score_dict = dict(zip(name, yearning))    # 굳이 list로 만들고 dictionary로 자료형 변환 필요 x
+    for p in photo:
+        score = 0
+        for i in p:
+            if i in score_dict:    # score_dict에 있는 값이면,
+                score += score_dict[i]    # 해당하는 value 더해준다.
+        answer.append(score)    # 합한 score를 answer 리스트에 추가
+    return answer
