d52f6337e08fffe4e310a4a70a00ca80d3e7bc66
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-07-18 21:07:33 +0900

    Update 폰켓몬.py

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,3 +1,4 @@
+# 내 풀이
 def solution(nums):
     pokemon_cnt = 0     # 가져갈 수 있는 pokemon 수 0으로 초기화
     N = len(nums)       # 연구실 내 pokemon 수
@@ -9,3 +10,7 @@ def solution(nums):
             break
             
     return pokemon_cnt
+
+# 다른 사람 풀이
+def solution(nums):
+    return min(len(nums) / 2, len(set(nums)))    # 전체 pokemon 수의 절반과 pokemon의 종류의 수 중 최솟값 반환
