1b88cddeb1387a3cdbd2e76cbc6f45138f0179d6
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-07-25 15:18:16 +0900

    Update 숫자 문자열과 영단어.py

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,3 +1,4 @@
+# dictionary를 활용한 풀이
 def solution(s):
     dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}    # 대응하는 영단어-숫자 딕셔너리 생성
 
@@ -5,3 +6,12 @@ def solution(s):
         s = s.replace(key, value)  # 문자열에 포함되는 key(영단어) 값을 value(숫자)로 replace
             
     return int(s)   # 문자열 데이터를 정수형으로 변환
+
+# list와 enumerate를 활용한 풀이
+def solution(s):
+    num_word_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for num, word in enumerate(num_word_list):    # 0 'zero', 1 'one', 2 'two' , ...
+        s = s.replace(word, str(num))    # 영단어를 숫자로 replace, replace 함수 사용을 위해 str으로 형변환
+
+    return int(s)    # 문자열 데이터를 정수형으로 변환
