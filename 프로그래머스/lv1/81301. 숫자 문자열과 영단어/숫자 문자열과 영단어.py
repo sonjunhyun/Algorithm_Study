4c6585554308284d5e39cfa7434e4995e30832e1
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-07-25 15:19:34 +0900

    Update 숫자 문자열과 영단어.py

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,6 +1,9 @@
 # dictionary를 활용한 풀이
 def solution(s):
-    dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}    # 대응하는 영단어-숫자 딕셔너리 생성
+    dic = {
+        'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+        'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+    }    # 대응하는 영단어-숫자 dictionary 생성
 
     for key, value in dic.items():
         s = s.replace(key, value)  # 문자열에 포함되는 key(영단어) 값을 value(숫자)로 replace
@@ -9,7 +12,7 @@ def solution(s):
 
 # list와 enumerate를 활용한 풀이
 def solution(s):
-    num_word_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    num_word_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']    # 0~9까지의 영단어 list 생성
 
     for num, word in enumerate(num_word_list):    # 0 'zero', 1 'one', 2 'two' , ...
         s = s.replace(word, str(num))    # 영단어를 숫자로 replace, replace 함수 사용을 위해 str으로 형변환
