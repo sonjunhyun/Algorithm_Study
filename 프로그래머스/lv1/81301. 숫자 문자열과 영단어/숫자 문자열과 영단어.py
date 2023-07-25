0105163d4e35fcfe78c357ce72b359168280e6a6
Jun Hyun Son <118335520+sonjunhyun@users.noreply.github.com>
2023-07-25 13:24:39 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,7 @@
 def solution(s):
-    dic = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0}
+    dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}    # 대응하는 영단어-숫자 딕셔너리 생성
 
     for key, value in dic.items():
-        s = s.replace(key, str(value))
+        s = s.replace(key, value)  # 문자열에 포함되는 key(영단어) 값을 value(숫자)로 replace
             
-    return int(s)
+    return int(s)   # 문자열 데이터를 정수형으로 변환
